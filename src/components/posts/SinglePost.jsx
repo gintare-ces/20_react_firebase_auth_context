4786e563b150f845ec8a1715dbb47851e52cf396
gintare-ces <gintare.cesiuliene@gmail.com>
2023-04-17 10:01:01 +0300

    singlePost

--- src/components/posts/SinglePost.jsx
+++ src/components/posts/SinglePost.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Card from '../ui/card/Card'
+import './singlePost.scss'
 
 function SinglePost({ item }) {
     
     
   return (
-    <li>
+    <li className='singlePost'>
         <Card>
             <h3>{item.title}</h3>
+            <p className="author">{item.author}</p>
             <p>{item.body}</p>
+            <p className="tags">{item.tags}</p>
+            <hr />
+            {/* <p className="date">{item.date}</p> */}
         </Card>
     </li>
   )
